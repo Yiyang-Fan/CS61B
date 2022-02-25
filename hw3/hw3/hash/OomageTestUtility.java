0389ca89cb4a352174230ab8f72a2ae72e78b389
Yiyang Fan <yiyang_fan@outlook.com>
2022-02-25 15:08:34 -0800

    update hw3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -12,6 +12,17 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        int N = oomages.size();
+        int[] buckets = new int[M];
+        for (Oomage o: oomages) {
+            int bucketNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            buckets[bucketNum] += 1;
+        }
+        for (int i = 0; i < M; i++) {
+            if (buckets[i] < N / 50.0 || buckets[i] > N / 2.5) {
+                return false;
+            }
+        }
+        return true;
     }
 }
