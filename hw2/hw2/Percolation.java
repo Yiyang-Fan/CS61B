a5a28a622fd3b4c09290fdc368b824b8e498a92e
Yiyang Fan <yiyang_fan@outlook.com>
2022-02-25 10:55:01 -0800

    update hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,90 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    WeightedQuickUnionUF FULL;
+    WeightedQuickUnionUF PERC;
+    int[][] OPEN;
+    int numOpen;
+    int indUp;
+    int indDown;
+    int N;
+    public Percolation(int N) {
+        FULL = new WeightedQuickUnionUF(N * N + 1);
+        PERC = new WeightedQuickUnionUF(N * N + 2);
+        OPEN = new int[N][N];
+        numOpen = 0;
+        indUp = N * N;
+        indDown = N * N + 1;
+        this.N = N;
+    }
 
+    public void open(int row, int col) {
+        if (OPEN[row][col] == 0) {
+            numOpen += 1;
+            OPEN[row][col] = 1;
+            connectOpenNeighbor(row, col);
+            if (isUpEdge(row, col)) {
+                PERC.union(getInd(row, col), indUp);
+                FULL.union(getInd(row, col), indUp);
+            }
+            if (isDownEdge(row, col)) {
+                PERC.union(getInd(row, col), indDown);
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        return OPEN[row][col] == 1;
+    }
+
+    public boolean isFull(int row, int col) {
+        return FULL.find(getInd(row, col)) == FULL.find(indUp);
+    }
+
+    public int numberOfOpenSites() {
+        return numOpen;
+    }
+
+    public boolean percolates() {
+        return PERC.find(indDown) == PERC.find(indUp);
+    }
+
+    private void connectOpenNeighbor(int row, int col) {
+        if (row + 1 < N) {
+            if (isOpen(row + 1, col)) {
+                PERC.union(getInd(row, col), getInd(row + 1, col));
+                FULL.union(getInd(row, col), getInd(row + 1, col));
+            }
+        }
+        if (row - 1 >= 0) {
+            if (isOpen(row - 1, col)) {
+                PERC.union(getInd(row, col), getInd(row - 1, col));
+                FULL.union(getInd(row, col), getInd(row - 1, col));
+            }
+        }
+        if (col + 1 < N) {
+            if (isOpen(row, col + 1)) {
+                PERC.union(getInd(row, col), getInd(row, col + 1));
+                FULL.union(getInd(row, col), getInd(row, col + 1));
+            }
+        }
+        if (col - 1 >= 0) {
+            if (isOpen(row, col - 1)) {
+                PERC.union(getInd(row, col), getInd(row, col - 1));
+                FULL.union(getInd(row, col), getInd(row, col - 1));
+            }
+        }
+    }
+
+    private boolean isDownEdge(int row, int col) {
+        return row == N - 1;
+    }
+
+    private int getInd(int row, int col) {
+        return row * N + col;
+    }
+
+    private boolean isUpEdge(int row, int col) {
+        return row == 0;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,42 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    double[] result;
+    StdRandom rand;
+    StdStats stat;
+    int T;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.T = T;
+        result = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation p = pf.make(N);
+            int k = 0;
+            while (!p.percolates()) {
+                int randRow = rand.uniform(N);
+                int randCol = rand.uniform(N);
+                while (p.isOpen(randRow, randCol)) {
+                    randRow = rand.uniform(N);
+                    randCol = rand.uniform(N);
+                }
+                p.open(randRow, randCol);
+                k += 1;
+            }
+            result[i] = k * 1.0 / (N * N * 1.0);
+        }
+    }
+    public double mean() {
+        return stat.mean(result);
+    }
+    public double stddev() {
+        return stat.stddev(result);
+    }
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+    public double confidenceUp() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 }
